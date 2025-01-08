106b7e41c146c1c8f45a5fc0d322bad8a38d3d28
TomJan <tomburniston@gmail.com>
2025-01-08 13:33:32 -0800

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,8 @@
+TomJan's Chess Improvment Tool (TCIT) is a multifaceted tool. It has the following features:
+
+ * Store Personal Games 
+ * Give statistics into performances in openings (EXPERIMENTAL IDEA: show quality of play in certain structures)
+ * Create Anki Deck for tactical mistakes from games
+ * Vizualization Training with blindfold tactics 
+
+
---
+++ readNewGames.py
@@ -0,0 +1,23 @@
+###### THIS READS NEW GAMES, COMPILES STATISTICS FOR THE GAMES AND ADDS THEM TO THE DATABASE
+###### READS FROM THE ./data/newGames.pgn file
+import chess.pgn
+import json
+
+f = open("./data/newGames.pgn")
+game = chess.pgn.read_game(f)
+output = open("./data/database.pgn", "a")
+
+while game:
+    output.write(str(game))
+    output.write("\n")
+    game = chess.pgn.read_game(f)
+    
+f.close()
+output.close()
+
+
+
+def generateStats(game): # here we want to add to the JSON file of stats
+    # use stockfish to generate a centipawn lose.
+    # get the eco code and extract it 
+
