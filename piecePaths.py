4ce31ff5fb9adbfc0cfda8402f9d36fe22016b12
TomJan <tomburniston@gmail.com>
2025-01-13 21:23:26 -0500

    Removed Temp Files & Started Blind Movement

---
+++ piecePaths.py
@@ -0,0 +1,11 @@
+import random
+## we will move pieces around blindfolded
+
+
+def knight(move): # will generate two squares, user will input series of squares to go from on to the next 
+    
+
+
+
+
+def bishop(): # will generate two random square, then the user will need to find the closes square connecting both
